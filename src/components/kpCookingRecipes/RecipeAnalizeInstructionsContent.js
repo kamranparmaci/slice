afb64250d2376ebdf2d19d00bc3084fd4c75f57c
kamrancode <kamranparmasii@gmail.com>
2021-06-01 01:46:10 +0430

    remove hooks folder and move fetch functions into there own component directly

--- src/components/kpCookingRecipes/RecipeAnalizeInstructionsContent.js
+++ src/components/kpCookingRecipes/RecipeAnalizeInstructionsContent.js
@@ -1,14 +1,9 @@
-import { useContext } from "react";
-
-import { SearchResults } from "../../providers/SearchResults";
 import RecipeInstructions from "./RecipeInstructions";
 
 const RecipeAnalizeInstructionsContent = () => {
-  const { recipeAnalizes } = useContext(SearchResults);
-
   return (
     <>
-      <RecipeInstructions {...recipeAnalizes} />
+      <RecipeInstructions />
     </>
   );
 };
--- src/providers/SearchResults.js
+++ src/providers/SearchResults.js
@@ -1,32 +1,16 @@
 import { useState } from "react";
 import { createContext } from "react";
-import useRecipes from "../hooks/useRecipes";
-import useRecipesAnalizes from "../hooks/useRecipesAnalizes";
 import DATA from "../data";
-import useRecipeIngredients from "../hooks/useRecipeIngredients";
-import useRecipeEquipments from "../hooks/useRecipeEquipments";
 
 export const RecipesContext = createContext(DATA);
 
 export const SearchResults = createContext({
   search: "",
   onHandleChange: () => {},
-  getData: () => {},
-  results: [],
-  recipeAnalizes: [],
-  ingredients: [],
-  equipments: [],
-  getRecipes: () => {},
-  getIngredients: () => {},
-  getEquipments: () => {},
 });
 
 const SearchResultsProvider = ({ children }) => {
   const [search, setSearch] = useState("");
-  const [results, getData] = useRecipes([]);
-  const [recipeAnalizes, getRecipes] = useRecipesAnalizes([]);
-  const [ingredients, getIngredients] = useRecipeIngredients([]);
-  const [equipments, getEquipments] = useRecipeEquipments([]);
 
   const onHandleChange = (e) => {
     setSearch(e.target.value);
@@ -37,14 +21,6 @@ const SearchResultsProvider = ({ children }) => {
       value={{
         search,
         onHandleChange,
-        getData,
-        results,
-        recipeAnalizes,
-        ingredients,
-        equipments,
-        getRecipes,
-        getIngredients,
-        getEquipments,
       }}
     >
       {children}
